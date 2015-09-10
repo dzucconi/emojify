bd09ebfcc7265cf6bfc0313d1a033f2ee7be8475
Charles Broskoski <broskoski@gmail.com>
2015-09-10 13:03:25 -0400

    sub before split

--- application.rb
+++ application.rb
@@ -18,7 +18,7 @@ class Application < Sinatra::Base
   post '/' do
 
     channel = params[:channel_id]
-    args = params[:text].split(" ").sub(":", "")
+    args = params[:text].sub(":", "").split(" ")
 
     client = Slack::Web::Client.new(token: ENV['SLACK_API_TOKEN'])
 
